e576546d2032f4c8439297c07ec867c1da83c174
Cristian Prieto <me@cprieto.com>
2013-07-28 12:39:23 +1000

    Now we have a helper for domain\user entry

---
+++ agent/src/org/tsqlt/runner/agent/DomainUser.java
@@ -0,0 +1,31 @@
+package org.tsqlt.runner.agent;
+
+import org.jetbrains.annotations.NotNull;
+
+public class DomainUser {
+    private final String user;
+    private final String domain;
+
+    public DomainUser(@NotNull String input){
+        if (input.contains("\\")){
+            String[] parsed = input.split("\\\\", 2);
+            user = parsed[1];
+            domain = parsed[0];
+        } else {
+            user = input;
+            domain = null;
+        }
+    }
+
+    public String getUser(){
+        return user;
+    }
+
+    public String getDomain(){
+        return domain;
+    }
+
+    public boolean hasDomain(){
+        return domain != null && !domain.isEmpty();
+    }
+}
---
+++ agent/tests/org/tsqlt/runner/agent/DomainUserTest.java
@@ -0,0 +1,43 @@
+package org.tsqlt.runner.agent;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class DomainUserTest {
+    @Test
+    public void testItCanGetServer(){
+        DomainUser sut = new DomainUser("user");
+
+        assertNull(sut.getDomain());
+        assertEquals(sut.getUser(), "user");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    @SuppressWarnings({"UnusedDeclaration", "ConstantConditions"})
+    public void testItThrowsIfInputIsNull(){
+        DomainUser sut = new DomainUser(null);
+    }
+
+    @Test
+    public void testItCanParseUserDomain(){
+        DomainUser sut = new DomainUser("domain\\user");
+
+        assertEquals(sut.getUser(), "user");
+        assertEquals(sut.getDomain(), "domain");
+    }
+
+    @Test
+    public void testItWouldTellYouYesWhenHasDomain(){
+        DomainUser sut = new DomainUser("domain\\user");
+
+        assertTrue(sut.hasDomain());
+    }
+
+    @Test
+    public void testItShouldTellYouNoWhenOnlyUser(){
+        DomainUser sut = new DomainUser("user");
+
+        assertFalse(sut.hasDomain());
+    }
+}
