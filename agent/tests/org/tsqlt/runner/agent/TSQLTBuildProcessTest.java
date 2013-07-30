9d72b5bd947b8c05623082f9c0621abdc824fbb0
Cristian Prieto <me@cprieto.com>
2013-07-30 22:36:15 +1000

    Adding mockito and starting a massive refactoring for the agent runner

---
+++ agent/tests/org/tsqlt/runner/agent/TSQLTBuildProcessTest.java
@@ -0,0 +1,29 @@
+package org.tsqlt.runner.agent;
+
+import jetbrains.buildServer.RunBuildException;
+import jetbrains.buildServer.agent.AgentRunningBuild;
+import jetbrains.buildServer.agent.BuildProcess;
+import jetbrains.buildServer.agent.BuildRunnerContext;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class TSQLTBuildProcessTest {
+    private BuildProcess sut;
+    private AgentRunningBuild agentRunningBuild;
+    private BuildRunnerContext context;
+
+    @BeforeTest
+    public void setUp(){
+        agentRunningBuild = mock(AgentRunningBuild.class);
+        context = mock(BuildRunnerContext.class);
+
+        sut = new TSQLTBuildProcess(agentRunningBuild, context);
+    }
+
+    @Test
+    public void testItShouldStartTask() throws RunBuildException {
+        sut.start();
+    }
+}
